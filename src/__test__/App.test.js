201f818249243196f4a7f232a4da0fd66ee1eab9
Balu Rathod <rathodbalu2@gmail.com>
2023-10-18 07:11:23 +0530

    code cleanup

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,12 +1,3 @@
-// import { render, screen, cleanup } from "@testing-library/react"; 
-// import App from "../App.jsx";
-// import '../App.css';
-
-// test('should render App component', () => {
-//   render(<App/>);
-//   const appEle = screen.getByTestId('app-1');
-//   expect(appEle).toBeInTheDocument();
-// })
 test('should render App component', () => {  
   expect(true).toBe(true);
 })
